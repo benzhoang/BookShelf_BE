33d81d710a94a868753cfc7a4e646b5b265c2e01
anhquan121203 <quannguyen121203@gmail.com>
2025-02-10 01:25:05 +0700

    fix config swagger and update swagger

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+// SETTUP SWAGGER
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
+const swaggerPath = path.join(__dirname, "swagger", "apiDocs.yaml");
+const swaggerDocument = YAML.load(swaggerPath);
+
 const app = express();
 const port = 3000;
-
-// Swagger Setup
-const setupSwagger = require("./configs/swagger.config");
 
 // Import Routers
 const BookRouter = require("./routers/book.routes");
@@ -17,24 +21,21 @@ const ActorRouter = require("./routers/actor.routes");
 const db = require("./configs/db.config");
 db.connect();
 
-
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Back-end project!!!" });
 });
 
-// Setup Swagger Documentation
-setupSwagger(app);
-
 // Setup Routers
 app.use("/api/books", BookRouter);
 app.use("/api/users", UserRouter);
-app.use("/api/bookMedias", BookMediaRouter);
+app.use("/api/bookmedias", BookMediaRouter);
 app.use("/api/actors", ActorRouter);
 
+// Swagger API DOCS
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
