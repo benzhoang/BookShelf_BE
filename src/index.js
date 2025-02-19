954d44a7795241c903ad6a03455bcec41b0e999c
PhongNLTSE171898 <phongnltse171898@fpt.edu.vn>
2025-02-19 21:39:51 +0700

    fix code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 console.log(process.env.NODE_ENV)
 
