c5abc6f924c09d3ba61764dd266796719f70daa4
PhongNLTSE171898 <phongnltse171898@fpt.edu.vn>
2025-03-12 13:46:53 +0700

    update code

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -3,17 +3,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-    // codeBook: {type: String},
+    codeBook: { type: String },
     bookName: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Schema.Types.Decimal128, required: true },
-    image: { type: String },
-    // soldBook: {type: Number, default: 0},
-    // quantity: {type: Number, required: true},
+    image: { type: String, required: true },
+    quantity: { type: Number, required: true },
     actorID: { type: Schema.ObjectId, ref: "Actor" },
     categoryID: { type: Schema.ObjectId, ref: "Category" },
     bookMediaID: { type: Schema.Types.ObjectId, ref: "BookMedia" },
-    
     available: { type: Boolean, default: true }
 }, { versionKey: false, timestamps: true })
 
