4158329f47479524f7961ffcc2c31a5b7f55a27f
anhquan121203 <quannguyen121203@gmail.com>
2025-03-26 21:36:57 +0700

    fix invoice

--- src/routers/invoiceDetails.routes.js
+++ src/routers/invoiceDetails.routes.js
@@ -1,19 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const InvoiceDetailsController = require("../controllers/invoiceDetails.controller")
+const InvoiceDetailsController = require("../controllers/invoiceDetails.controller");
 
 const {
-    authenticate,
-    authorizeAdmin,
-    authorizeCustomer,
-    authorizeManager,
-    authorizeStaff,
+  authenticate,
+  authorizeAdmin,
+  authorizeCustomer,
+  authorizeManager,
+  authorizeStaff,
 } = require("../middlewares/auth.middleware");
 
-router.get("/", authenticate, authorizeAdmin, InvoiceDetailsController.getAllInvoiceDetails);
-router.get("/:id", authenticate, authorizeAdmin, InvoiceDetailsController.getInvoiceDetailsById);
-router.post("/", authenticate, authorizeCustomer, InvoiceDetailsController.createInvoiceDetails);
-router.delete("/:id", authenticate, authorizeCustomer, InvoiceDetailsController.deleteInvoiceDetails);
-router.put("/:id", authenticate, authorizeCustomer, InvoiceDetailsController.updateInvoiceDetails);
+router.get(
+  "/",
+  authenticate,
+  authorizeAdmin,
+  InvoiceDetailsController.getAllInvoiceDetails
+);
+router.get("/:id", authenticate, authorizeCustomer, InvoiceDetailsController.getInvoiceDetailsById);
+router.post(
+  "/",
+  authenticate,
+  authorizeCustomer,
+  InvoiceDetailsController.createInvoiceDetails
+);
+router.delete(
+  "/:id",
+  authenticate,
+  authorizeCustomer,
+  InvoiceDetailsController.deleteInvoiceDetails
+);
+router.put(
+  "/:id",
+  authenticate,
+  authorizeCustomer,
+  InvoiceDetailsController.updateInvoiceDetails
+);
 
 module.exports = router;
